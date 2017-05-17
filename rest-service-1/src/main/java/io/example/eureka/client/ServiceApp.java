ce584717cf5b26aeb9131339936c23424614df1b
Sergei Malafeev <sergeymalafeev@gmail.com>
2017-05-17 16:48:37 +0800

    in progress

--- rest-service-1/src/main/java/io/example/eureka/client/ServiceApp.java
+++ rest-service-1/src/main/java/io/example/eureka/client/ServiceApp.java
@@ -2,31 +2,32 @@ package io.example.eureka.client;
 
 
 import com.lightstep.LightStepConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @EnableEurekaClient
 @RestController
 @Import(LightStepConfiguration.class)
+@SpringBootApplication
 public class ServiceApp {
 
-    @RequestMapping("/")
-    public String home(@RequestHeader HttpHeaders headers) {
-        return "Hello from Service 1";
-    }
+  @RequestMapping("/")
+  public String index(@RequestHeader HttpHeaders headers) {
+    return "Hello from Service 1 Index";
+  }
 
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(ServiceApp.class).web(true).run(args);
-    }
+  @RequestMapping("/home")
+  public String home(@RequestHeader HttpHeaders headers) {
+    return "Hello from Service 1 Home";
+  }
+
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(ServiceApp.class).web(true).run(args);
+  }
 }
--- rest-service-2/src/main/java/io/example/eureka/client2/ServiceApp2.java
+++ rest-service-2/src/main/java/io/example/eureka/client2/ServiceApp2.java
@@ -3,23 +3,22 @@ package io.example.eureka.client2;
 
 import com.lightstep.LightStepConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @EnableEurekaClient
 @RestController
 @Import(LightStepConfiguration.class)
+@SpringBootApplication
 public class ServiceApp2 {
 
   private final StoreClient storeClient;
@@ -29,14 +28,26 @@ public class ServiceApp2 {
     this.storeClient = storeClient;
   }
 
-  @RequestMapping("/")
-  public String home() {
+  @Bean
+  @LoadBalanced
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @GetMapping("/")
+  public String index() {
     return storeClient.get();
+  }
+
+  @GetMapping("/home")
+  public String home() {
+    return restTemplate.getForObject("http://rest-service-1/home", String.class);
   }
 
   public static void main(String[] args) {
     new SpringApplicationBuilder(ServiceApp2.class).web(true).run(args);
   }
-
-
 }
